f8d4b1ca38c47b62b57b432b8eeb97d3d2c7d5aa
DollyVolley <oliver.schwarz@block42.tech>
2020-07-14 16:45:22 +0200

    First Metric Test

---
+++ README.md
@@ -0,0 +1,2 @@
+# Lachesis Exporter
+
--- collector.go
+++ collector.go
@@ -1,23 +1,53 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
 	"github.com/prometheus/client_golang/prometheus"
 )
 
-var fooMetric = prometheus.NewGauge(prometheus.GaugeOpts{
-	Name: "foo_metric", Help: "Shows whether a foo has occurred in our cluster"})
+// TODO: Heavy Refactor!
+var currentEpoch = prometheus.NewSummary(prometheus.SummaryOpts{
+	Name: "current_epoch", Help: "Current epoch number"})
 
-var barMetric = prometheus.NewGauge(prometheus.GaugeOpts{
-	Name: "bar_metric", Help: "Shows whether a bar has occurred in our cluster"})
+const url = "http://localhost:18545"
+
+// GetEpochRequestBody For evaluating the current epoch
+func GetEpochRequestBody() ([]byte, error) {
+	return json.Marshal(&struct {
+		JSONRPC string `json:"jsonrpc"`
+		Method  string `json:"method"`
+		ID      int64  `json:"id"`
+	}{
+		JSONRPC: "2.0",
+		Method:  "ftm_currentEpoch",
+		ID:      1,
+	})
+}
+
+func getBlockHeight() int16 {
+	header := "application/json"
+	body, _ := GetEpochRequestBody()
+	response, err := http.Post(url, header, bytes.NewBuffer(body))
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		data, _ := ioutil.ReadAll(response.Body)
+		fmt.Println(string(data))
+	}
+
+	return 1
+}
+
+func main() {
+	getBlockHeight()
+}
 
 func init() {
-	//Register metrics with prometheus
-	prometheus.MustRegister(fooMetric)
-	prometheus.MustRegister(barMetric)
-
-	//Set fooMetric to 1
-	fooMetric.Set(0)
-
-	//Set barMetric to 0
-	barMetric.Set(1)
+	prometheus.MustRegister(currentEpoch)
 }
