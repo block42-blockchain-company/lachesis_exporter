974b89b770f799e0cfa02a41efdc21783a1001e5
DollyVolley <oliver.schwarz@block42.tech>
2020-07-14 15:19:31 +0200

    Init exporter

---
+++ collector.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+var fooMetric = prometheus.NewGauge(prometheus.GaugeOpts{
+	Name: "foo_metric", Help: "Shows whether a foo has occurred in our cluster"})
+
+var barMetric = prometheus.NewGauge(prometheus.GaugeOpts{
+	Name: "bar_metric", Help: "Shows whether a bar has occurred in our cluster"})
+
+func init() {
+	//Register metrics with prometheus
+	prometheus.MustRegister(fooMetric)
+	prometheus.MustRegister(barMetric)
+
+	//Set fooMetric to 1
+	fooMetric.Set(0)
+
+	//Set barMetric to 0
+	barMetric.Set(1)
+}
